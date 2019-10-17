5d39b4c4d4e9f7fd3b3deca93c81ec6723d8c9d9
as-chapa <e.goromgorom@gmail.com>
2019-10-18 06:57:32 +0900

    詳細ページまで参照してサマリを表示するように修正

--- study_requests-html.py
+++ study_requests-html.py
@@ -9,3 +9,10 @@ print(r.html.url)
 items = r.html.find(".topicsListItem", first=False)
 for item in items:
     print(item.text)
+    # 重複を除くためset型で返されるのでlistに変換しておく
+    link = list(item.absolute_links)
+    
+    # 詳細ページのサマリ表示する
+    r_detail = session.get(link[0])
+    item_detail = r_detail.html.find(".tpcNews_summary", first=True)
+    print(item_detail.text)
